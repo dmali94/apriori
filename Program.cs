e2fd5a09574631b229670386a5aa67b51efb490e
Ali Najafi <zero9196113387@gmail.com>
2016-04-24 21:35:24 +0430

    scans
    other scans  for transction...

--- Program.cs
+++ Program.cs
@@ -56,6 +56,75 @@ namespace apriori_sch
                }
            }
        }
+
+      static void build(int supmin)
+      {
+          int start = 0;
+          bool t=false;
+          string key="";
+          while (c - start != 1)
+          {
+              for (int i = start; i < ctotal; i++)         ///// az aval ta akhare  arraye ra pymayesh mikonim ...
+              {
+                  for (int j = i + 1; j < ctotal; j++)    /////// az yeki payeentarn sateh moghayese ro anjam midamim...
+                  {
+                      for (int k = 0, ki = 0; k < ans[i].counts; k++, ki++)      //// ta akhare majmohe i jelo mirim va dar akhr ham haman majmoe ra ham azafe mikonim...
+                      {
+                          ans[++ctotal].item[ki] = ans[i].item[k];
+                          for (int g = 0;g < ans[j].counts; g++)
+                          {
+                               key = ans[j].item[0];
+                              if (key == ans[i].item[g])
+                              {
+                                  t = true;
+                                  break;
+                              }
+
+                          }
+                          if (!t)
+                          {
+                              ans[ctotal] = new items();
+                              ans[ctotal++].item[ki++] = key;
+                          }
+                          t = false;
+                          findCount(key,supmin);
+                      }
+                  }
+
+              }
+          }
+      }
+      static void findCount(string s,int supmin)
+      {
+          int count = 0;
+
+          for (int i = 0; i < c; i++)
+          {
+              for (int j = 0; j < cpylist[i].counts; j++)
+              {
+                  string key = s;
+                  for (int k = 0; k < c; k++)
+                  {
+                      for (int h = 0; h < list[k].counts; h++)
+                      {
+                          if (key == list[k].item[h] && list[k].item[h] != "")
+                          {
+                              count++;
+                              
+                          }
+                      }
+                  }
+                  if (count >= supmin)
+                  {
+                      ans[ctotal] = new items();
+                      ans[ctotal].item[0] = key;
+                      ans[ctotal++].counts = count;
+                      count = 0;
+                  }
+              }
+          }
+ 
+      }
        static void Main(string[] args)
         {
 
@@ -85,6 +154,8 @@ namespace apriori_sch
             }
             /////////////////////////
             scan_1th(min);
+            build(min);
+            Console.WriteLine(ans[ctotal].item+ans[ctotal].counts);
             
         }
     }
